9d789452f3756595636d05ebf99265232120fae3
Dhanasekar Murugesan <beingdhanasekar@outlook.com>
2023-03-06 16:40:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using ElsaQuickstarts.Server.Dashboard;
+
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup();
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app, builder.Environment);
+
+app.Run();
